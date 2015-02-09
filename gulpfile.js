101154b4f89f20164d8905288bd58ad102204389
Gijs van de Nieuwegiessen <gijs@nieuwegiessen.nl>
2015-02-09 23:25:20 +0100

    Echte initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,9 @@ var gulp = require('gulp'),
     fm = require('front-matter'),
     data = require('gulp-data'),
     jade = require('gulp-jade'),
-    sass = require('gulp-ruby-sass'),
-    minifycss = require('gulp-minify-css'),
+    libsass = require('gulp-sass'),
+    minifycss= require('gulp-minify-css'),
     notify = require('gulp-notify'),
-    imagemin = require('gulp-imagemin'),
     newer = require('gulp-newer'),
     browserSync = require('browser-sync'),
     reload = browserSync.reload,
@@ -20,7 +19,12 @@ var gulp = require('gulp'),
     rename = require('gulp-rename'),
     consolidate = require('gulp-consolidate'),
     sourcemaps = require('gulp-sourcemaps'),
-    deploy = require('gulp-gh-pages');
+    cache = require('gulp-cached'),
+    jadeAffected = require('gulp-jade-find-affected'),
+    postcss = require('gulp-postcss'),
+    minimist = require('minimist'),
+    mqpacker = require('css-mqpacker'),
+    pixrem = require('gulp-pixrem');
  
 var paths = {
   templates: './src/jade/',
@@ -30,37 +34,64 @@ var paths = {
   scripts: './src/js/'
 };
 
-// Clean: remove all build output
+
+// ******************************
+// CLI argumenten
+// ******************************
+var knownOptions = {
+  string: 'env',
+  default: { env: 'development' }
+};
+
+var options = minimist(process.argv.slice(2), knownOptions);
+
+// ******************************
+// Dist map leegmaken
+// ******************************
 gulp.task('clean', function(cb) {
   // You can use multiple globbing patterns as you would with `gulp.src`
   del(['dist'], cb);
 });
 
-// Bower: install components
+// ******************************
+// Bower componenten installeren
+// ******************************
 gulp.task('bower', function() {
   return bower()
 });
 
-// Preen: clean Bower components
 gulp.task('preen', ['bower'], function(cb) {
   preen.preen({}, cb);
 });
 
-// Scripts: Uglify, concat
+// ******************************
+// Custom Javascripts 
+// ******************************
 gulp.task('scripts', function() {
   return gulp.src(path.join(paths.scripts, '**/*.js'))
+    .pipe(cache('js'))
     .pipe(sourcemaps.init())
-    .pipe(concat('interpolis.min.js'))
+    .pipe(concat('scripts.min.js'))
     .pipe(uglify())
+    .on('error', function(error) {
+      console.log(error);
+      this.emit('end');
+    })
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('./dist/js'))
     .pipe(notify({ title: 'Gulp: scripts updated', message: '<%= file.relative %>' }));
 });
   
-// Jade: grab partials from templates and render out html files
+// ******************************
+// Jade (HTML) templates
+// ******************************
 gulp.task('jade', function() {
-  gulp.src(['./src/jade/**/*.jade', '!./src/jade/layout/*.jade'])    
-   
+    
+  return gulp.src(['./src/jade/**/*.jade', '!./src/jade/layout/*.jade'])
+    
+    // Filter unaffected
+    .pipe(jadeAffected())
+    
     // Front matter
     .pipe(data(function(file) {
       var content = fm(String(file.contents));
@@ -88,37 +119,63 @@ gulp.task('jade', function() {
     .pipe(notify({ title: 'Gulp: Jade compiled', message: '<%= file.relative %>' }));
 });
     
-// Images static images
+// ******************************
+// Statische plaatjes 
+// ******************************
 gulp.task('images', function() {
-  gulp.src(paths.images)
+  return gulp.src(paths.images)
     // Pass in options to the task
+    .pipe(cache('images'))
     .pipe(newer('./dist/images/'))
-    .pipe(imagemin({optimizationLevel: 5}))
     .pipe(gulp.dest('./dist/images/'))
     .pipe(reload({stream:true}));
 });
 
-// Compass: compile sass to css
-gulp.task('compass', function() {
-  gulp.src(path.join(paths.sass, '**/*.scss'))
-    .pipe(newer('./dist/css/'))
-    .pipe(sass({compass: true, require: ['susy']}))
-    .on('error', function(error) {
-      // Would like to catch the error here
-      console.log(error);
-      this.emit('end');
-    })
-    .pipe(gulp.dest('./dist/css/'))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(minifycss())
-    .pipe(gulp.dest('./dist/css/'))
-    .pipe(reload({stream:true}))
-    .pipe(notify({ title: 'Gulp: Sass compiled', message: '<%= file.relative %>' }));
+
+// ******************************
+// Sass (SCSS)
+// ******************************
+gulp.task('sass', function() {
+  
+  var stream = gulp.src(path.join(paths.sass, '**/*.scss'));
+  
+  if (options.env !== 'production') {
+    stream = stream.pipe(sourcemaps.init());
+  }
+  
+  stream = stream.pipe(libsass({includePaths: require('node-bourbon').with('./node_modules/susy/sass/'),
+                 errLogToConsole: true,
+                     outputStyle: 'nested',
+                  sourceComments: (options.env !== 'production')
+  }))
+  .on('error', function(error) {
+    // Would like to catch the error here
+    console.log(error);
+    this.emit('end');
+  });
+  
+  if (options.env !== 'production') {
+    stream = stream.pipe(sourcemaps.write('./'))
+                   .pipe(gulp.dest('./dist/css/'));
+  }
+  else {  
+   stream = stream.pipe(postcss([mqpacker]))
+                  .pipe(minifycss())
+                  .pipe(gulp.dest('./dist/css/'))
+                  .pipe(pixrem())
+                  .pipe(rename({suffix: ".old"}))
+                  .pipe(gulp.dest('./dist/css/'));   
+  }
+  
+  return stream.pipe(reload({stream:true}))
+               .pipe(notify({ title: 'Gulp: Sass compiled', message: '<%= file.relative %> <%= options.env %>' }));
 });
 
 
-// Browser-sync: Local webserver
-gulp.task('browser-sync', ['compass'], function() {
+// ******************************
+// Lokale webserver
+// ******************************
+gulp.task('browser-sync', function() {
   browserSync({
     server: {
       baseDir: './dist/'
@@ -126,31 +183,33 @@ gulp.task('browser-sync', ['compass'], function() {
   });
 });
 
-// Sketch: export sketch slice
+// ******************************
+// Sketch
+// ******************************
 // Install http://bohemiancoding.com/sketch/tool/
+// sketchtool export slices ./src/sketch/design.sketch --output=./dist/images
 gulp.task('sketch', function(){
   return gulp.src(path.join(paths.sketch, '*.sketch'))
     .pipe(sketch({
       export: 'slices',
       clean: true
     }))
-    .on('error', function(error) {
-      console.log(error);
-      this.emit('end');
-    })
     .pipe(gulp.dest('./dist/images/'));
 });
 
-gulp.task('icon-font', ['sketch'], function(){
+// ******************************
+// Icon font
+// ******************************
+gulp.task('icon-font', function(){
   var template = 'sass-style';
   
   return gulp.src('./dist/images/icons/*.svg')
-    .pipe(iconfont({ fontName: 'interpolis-icons' }))
+    .pipe(iconfont({ fontName: 'icons' }))
     .on('codepoints', function(codepoints) {
       
       var options = {
         glyphs: codepoints,
-        fontName: 'interpolis-icons',
+        fontName: 'icons',
         fontPath: '/fonts/', // set path to font (from your CSS file if relative)
         className: 'icon' // set class name in your CSS
       };
@@ -158,7 +217,16 @@ gulp.task('icon-font', ['sketch'], function(){
       gulp.src('./src/sketch/iconfont-templates/' + template + '.scss')
         .pipe(consolidate('lodash', options))
         .pipe(rename({ basename: '_icons' }))
-        .pipe(gulp.dest('./src/css/interpolis/base/'))
+        .pipe(gulp.dest('./src/css/base/'))
+        .on('error', function(error) {
+          console.log(error);
+          this.emit('end');
+        })
+      
+      gulp.src('./src/sketch/iconfont-templates/partial.jade')
+        .pipe(consolidate('lodash', options))
+        .pipe(rename({ basename:'icons' }))
+        .pipe(gulp.dest('./src/jade/partials/'))
         .on('error', function(error) {
           console.log(error);
           this.emit('end');
@@ -180,29 +248,49 @@ gulp.task('icon-font', ['sketch'], function(){
     .pipe(gulp.dest('./dist/fonts/'));
 });
  
+// ******************************
 // Default Gulp Task
-gulp.task('default', ['bower',
-                      'images',
-                      'scripts',
+// ******************************
+gulp.task('default', ['scripts',
                       'jade',
-                      'compass', 
-                      'preen', 
-                      'browser-sync'], function() {
+                      'sass', 
+                      'browser-sync',
+                      'images'], function() {
   
   // Watchers
-  gulp.watch(path.join(paths.templates, '**/*.jade'), ['jade']);
-  gulp.watch(path.join(paths.sass, '**/*.scss'), ['compass']);
+  gulp.watch('./src/jade/**/*.jade', ['jade']);
+  gulp.watch(path.join(paths.sass, '**/*.scss'), ['sass']);
   gulp.watch(path.join(paths.scripts, '**/*.js'), ['scripts']);
   gulp.watch(paths.images, ['images']);
 });
 
-// Default Gulp Task
+// ******************************
+// Export sketch Gulp Task
+// ******************************
 gulp.task('export-sketch', ['sketch',
                             'icon-font'], function() {
 });
+
+// ******************************
+// Install components task
+// ******************************
+gulp.task('install', ['bower',
+                      'preen'], function() {
+  
+});
+
+// ******************************
+// CSS Lint task
+// ******************************
+var csslint = require('gulp-csslint');
  
-// Deploy to github pages
-gulp.task('deploy', function () {
-  return gulp.src('./dist/**/*')
-    .pipe(deploy())
+gulp.task('css', function() {
+  /*csslint adjoining-classes:false */
+
+  gulp.src('dist/css/*.css')
+    .pipe(csslint({
+      'adjoining-classes': false,
+      'box-sizing': false
+    }))
+    .pipe(csslint.reporter());
 });
