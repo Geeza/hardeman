0825db285020e1c45e040ec45c78ad5574f6f5bf
Gijs van de Nieuwegiessen <gijs@nieuwegiessen.nl>
2015-02-16 00:03:10 +0100

    Production release

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 
-if [ -z "$1" ]
-then
-  echo "Which folder do you want to deploy to GitHub Pages?"
-  exit 1
-fi
 gulp jade --env production
 gulp sass --env production
 git commit -a -m "Production release"
 git push origin master
-git subtree push --prefix $1 origin gh-pages
+git subtree push --prefix ./dist origin gh-pages
