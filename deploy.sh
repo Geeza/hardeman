ccea4da8797349eefe9fa208d2442af6c880f428
Gijs van de Nieuwegiessen <gijs@nieuwegiessen.nl>
2015-02-16 00:09:06 +0100

    Production release

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ gulp jade --env production
 gulp sass --env production
 git commit -a -m "Production release"
 git push origin master
-git subtree push --prefix ./dist origin gh-pages
+git subtree push --prefix dist origin gh-pages
